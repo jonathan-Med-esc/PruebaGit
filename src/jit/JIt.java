d152aaba0e156624a7708ec6210513c4a615693c
jhon <Jhon.@gmail.com>
2026-04-16 17:11:52 +0200

    primera vez

---
+++ src/jit/JIt.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package jit;
+
+/**
+ *
+ * @author jonathan.medesc
+ */
+public class JIt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+              System.out.println(" asdasdasd");
+        }
+      
+    }
+    
+}
