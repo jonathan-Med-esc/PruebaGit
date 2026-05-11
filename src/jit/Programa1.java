df1b7cac12f74cc090e14dca21a519e4ed29ab66
jhon <Jhon.@gmail.com>
2026-05-11 18:34:32 +0200

    nueva clase

---
+++ src/jit/Programa1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package jit;
+
+/**
+ *
+ * @author jonathan.medesc
+ */
+public class Programa1 {
+    
+    public static void main(String[] args) {
+        System.out.println("113123");
+        
+    }
+}
